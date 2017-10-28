9a2d2902f4abbae29e62e8c64a81a2619fb3cf1f
Carl Johan Hanberg <cjh@amcentret.dk>
2017-10-28 14:07:44 +0200

    catch works

--- Assets/Catch.cs
+++ Assets/Catch.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 public class Catch: MonoBehaviour {
 	public RectTransform bar;
+	public RectTransform target;
 	public RectTransform marker;
-	bool isCatchAble;
+	public float speed = 20f;
+	public bool isCatchAble;
+
+	Coroutine co;
 
 	void OnEnable() {
-		
+		co = StartCoroutine (MarkerRoutine());
 	}
 
-	/*IEnumerator MarkerRoutine() {
-		while (true) {
-			Vector2 dir = Vector2.right;
-			if(marker.anchoredPosition.x >= bar.rect.
-			marker.anchoredPosition += Vector2.right;
+	public bool Throw() {
+		if(co!=null)
+			StopCoroutine (co);
+		return isCatchAble;
+	}
 
+	IEnumerator MarkerRoutine() {
+		float barSizeX = bar.rect.size.x/2f;
+		float targetSizeX = target.rect.size.x / 2f;
+		Vector2 dir = Vector2.right;
+		while (true) {
+			if(marker.anchoredPosition.x >= bar.anchoredPosition.x+barSizeX) {
+				dir = Vector2.left;
+			} else if (marker.anchoredPosition.x <= bar.anchoredPosition.x-barSizeX) {
+				dir = Vector2.right;
+			}
+			if (marker.anchoredPosition.x <= target.anchoredPosition.x + targetSizeX && marker.anchoredPosition.x >= target.anchoredPosition.x - targetSizeX) {
+				isCatchAble = true;
+			} else {
+				isCatchAble = false;
+			}
+			marker.anchoredPosition += dir*Time.deltaTime*speed;
+			yield return null;
 		}
-	}*/
+	}
 }
