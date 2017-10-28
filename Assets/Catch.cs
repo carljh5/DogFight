5333464b8066e9dced0b7f7c7d21602beac4b2fd
Carl Johan Hanberg <cjh@amcentret.dk>
2017-10-28 13:50:15 +0200

    bla

---
+++ Assets/Catch.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catch: MonoBehaviour {
+	public RectTransform bar;
+	public RectTransform marker;
+	bool isCatchAble;
+
+	void OnEnable() {
+		
+	}
+
+	/*IEnumerator MarkerRoutine() {
+		while (true) {
+			Vector2 dir = Vector2.right;
+			if(marker.anchoredPosition.x >= bar.rect.
+			marker.anchoredPosition += Vector2.right;
+
+		}
+	}*/
+}
