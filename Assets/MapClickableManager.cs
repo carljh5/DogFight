481da65d8afbf88d100678887b912b1f9b91179c
Carl Johan Hanberg <cjh@amcentret.dk>
2017-11-09 10:36:29 +0100

    UI stuff

--- Assets/MapClickableManager.cs
+++ Assets/MapClickableManager.cs
@@ -33,9 +33,10 @@ public class MapClickableManager : MonoBehaviour {
 		}
 
 		foreach(ClickableProperty clickProp in clickableProperties) {
-			Clickable clone = Instantiate (clickable, 
+			Clickable clone = Instantiate (clickable, clickable.transform.parent);
+			/*Clickable clone = Instantiate (clickable, 
 				new Vector3 (UnityEngine.Random.Range (50f, Screen.height-400f), UnityEngine.Random.Range (50, Screen.width-300f)), 
-					Quaternion.identity, clickable.transform.parent);
+					Quaternion.identity, clickable.transform.parent);*/
 			clickables.Add (clone);
 
 			Sprite tempSpr = null;
@@ -60,7 +61,23 @@ public class MapClickableManager : MonoBehaviour {
 	}
 
 	IEnumerator SpawnRoutine() { 
+		RectTransform clickableRT = clickable.GetComponent<RectTransform> ();
+		float clickableWidth = clickableRT.rect.width;
+		float clickableHeight = clickableRT.rect.height;
+		float padding = 50f;
+		int divX = Mathf.FloorToInt(Screen.width / clickableWidth);
+		int divY = Mathf.FloorToInt (Screen.height/clickableHeight);
+		List<Vector3> positions = new List<Vector3>();
+		for (int x = 0; x < divX; x++) {
+			for (int y = 0; y < divY; y++) {
+				positions.Add (new Vector3((x*clickableWidth)+padding, (y*clickableHeight)+padding));
+			}
+		}
+
 		foreach(Clickable click in clickables) {
+			int i = UnityEngine.Random.Range (0,positions.Count);
+			click.transform.position = positions [i];
+			positions.RemoveAt (i);
 			click.gameObject.SetActive (true);
 			yield return new WaitForSeconds (0.5f);
 		}
