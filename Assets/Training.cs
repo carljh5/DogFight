d223af2a4188851dc4f84198f23b8f1e6d1f2468
Paw Høvsgaard <pawhovsgaard@gmail.com>
2017-11-09 15:14:04 +0100

    Training ending

--- Assets/Training.cs
+++ Assets/Training.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class Training : MonoBehaviour {
 	public Image fill;
 	public float decay = 10f;
-	public GameObject scene;
+    public GameObject successScene, failureScene;
 	float maxVal = 100f;
 	float minVal = 0f;
 	float curVal = 50f;
@@ -34,25 +34,31 @@ public class Training : MonoBehaviour {
 				fill.fillAmount = curVal / 100f;
 			if (curVal >= maxVal) {
 				gameObject.SetActive (false);
-				scene.SetActive (true);
+				successScene.SetActive (true);
+			    string str = "";
 			    switch (Type)
 			    {
                     case TrainingType.Aggression:
 			            GameManager.PlayerDog.aggression++;
+			            str = "AGGRESSION";
 			            break;
                     case TrainingType.Bite:
 			            GameManager.PlayerDog.bite++;
-			            break;
+                        str = "BITE";
+                        break;
                     case TrainingType.Speed:
 			            GameManager.PlayerDog.speed++;
+                        str = "SPEED";
                         break;
                     case TrainingType.Strength:
 			            GameManager.PlayerDog.strength++;
-			            break;
+                        str = "STRENGTH";
+                        break;
                     default:
 			            break;
 
 			    }
+			    successScene.GetComponent<Text>().text = GameManager.Clean("SUCCESS! @dogName gained 1 " + str);
 
 				break;
 			}
@@ -60,7 +66,7 @@ public class Training : MonoBehaviour {
 			yield return null;
 			if (curVal <= minVal) {
 				gameObject.SetActive (false);
-				scene.SetActive (true);
+				failureScene.SetActive (true);
 				break;
 			}
 		}
---
+++ Assets/WaitThenGoToScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitThenGoToScene : MonoBehaviour
+{
+    public int SecondsToWait = 4;
+    public GameObject Scene;
+
+	void OnEnable ()
+	{
+	    StartCoroutine(Routine());
+	}
+
+    IEnumerator Routine()
+    {
+        yield return new WaitForSeconds(SecondsToWait);
+        Scene.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
