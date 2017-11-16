8986643eb9865b79dd339bd22244c9e447bf9416
Paw Høvsgaard <pawhovsgaard@gmail.com>
2017-11-16 16:27:08 +0100

    Autopick option script

---
+++ Assets/ActionPickOption.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    
+public class ActionPickOption : MonoBehaviour
+{
+    [Range(0, 10)]
+    public float SecondsBeforeAutoPick;
+
+    public GameObject[] panelsToDeactivate;
+
+    public FightManager Manager;
+
+    void OnEnable()
+    {
+        StartCoroutine(PickOptionRoutine());
+    }
+
+    IEnumerator PickOptionRoutine()
+    {
+        yield return new WaitForSeconds(SecondsBeforeAutoPick);
+
+        foreach (var p in panelsToDeactivate)
+        {
+            p.SetActive(false);
+        }
+
+        Manager.Round(FightManager.FightAction.NoAction);
+    }
+}
