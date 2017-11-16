41c9607a385ca57dfc99140e9d66f59b163e6145
Carl Johan Hanberg <cjh@amcentret.dk>
2017-11-16 10:56:53 +0100

    Lækkerier

---
+++ Assets/AccellerometerParallax.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccellerometerParallax : MonoBehaviour {
+	Vector3 origPos;
+
+	void Start() {
+		origPos = transform.position;
+	}
+		
+	Vector3 velocity = Vector3.zero;
+
+	void Update () {
+		Vector3 targetPosition = origPos + ((Quaternion.AngleAxis(45, Vector3.right) * Input.acceleration)* 50f); 
+		transform.position = Vector3.SmoothDamp (transform.position, targetPosition, ref velocity, 0.3f);
+		print (transform.position);
+	}
+}
