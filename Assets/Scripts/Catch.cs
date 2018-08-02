3694265cce03d7c5126da8be4c5d21cd69ecd29e
Paw Høvsgaard <pawhovsgaard@gmail.com>
2018-08-02 13:03:04 +0200

    Catch the different dogs

--- Assets/Scripts/Catch.cs
+++ Assets/Scripts/Catch.cs
@@ -1,32 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Catch: MonoBehaviour {
 	public RectTransform bar;
 	public RectTransform target;
 	public RectTransform marker;
 	public float speed = 20f;
-	public bool isCatchAble;
     public Animator LassoAnimator;
     public GameObject FailScene;
+    public GameObject SuccessScene;
+    public List<Dog> CatchDogs;
+    private Dog dogToCatch;
+    public Image DogImage;
+
 
 	Coroutine co;
+    private bool isCatchAble;
 
-	void OnEnable() {
+    void OnEnable() {
+        dogToCatch = CatchDogs[Random.Range(0, CatchDogs.Count - 1)];
+        DogImage.sprite = dogToCatch.sprite;
+
 		co = StartCoroutine (MarkerRoutine());
 	}
 
 	public void Throw() {
 		if(co!=null)
 			StopCoroutine (co);
-        
-        LassoAnimator.Play("LassoThrow");
-	    StartCoroutine(SwitchToScene());
+
+        if (isCatchAble && Random.Range(0, 30) >( dogToCatch.strength+dogToCatch.speed))
+        {
+            Debug.Log("caught the " + dogToCatch.race);
+            StartCoroutine(SwitchToSuccesScene());
+        }
+        else
+            StartCoroutine(SwitchToFailScene());
 	}
 
-    public IEnumerator SwitchToScene()
+    public IEnumerator SwitchToFailScene()
     {
+        LassoAnimator.Play("LassoThrow");
         SoundManager.PlayThrow();
         yield return new WaitForSeconds(1);
 
@@ -34,7 +49,23 @@ public class Catch: MonoBehaviour {
         gameObject.SetActive(false);
     }
 
-	IEnumerator MarkerRoutine() {
+
+    public IEnumerator SwitchToSuccesScene()
+    {
+        LassoAnimator.Play("LassoThrow");
+        SoundManager.PlayThrow();
+        yield return new WaitForSeconds(1);
+
+        //Remove dog from catchables
+
+        //add dog to players roster
+
+        SuccessScene.SetActive(true);
+        gameObject.SetActive(false);
+        
+    }
+
+    IEnumerator MarkerRoutine() {
 		float barSizeX = bar.rect.size.x/2f;
 		float targetSizeX = target.rect.size.x / 2f;
 		Vector2 dir = Vector2.right;
