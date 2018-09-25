822499827384d334ba747416fd45a13869368444
Carl <carl@carljh.dk>
2018-09-25 12:43:37 +0200

    tutorials soon finished

---
+++ Assets/TutorialOnDisableScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialOnDisableScript : MonoBehaviour {
+
+    public GameObject tutorialToToggle;
+    private bool hasBeenToggled;
+
+    private void OnDisable()
+    {
+        if(!hasBeenToggled) {
+            tutorialToToggle.SetActive(true);
+            hasBeenToggled = true;
+        }
+    }
+}
